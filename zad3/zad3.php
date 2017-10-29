ce687c9d05c80dd82a383eb3b77b8718e8cfa792
Osuchov <osuchto@wp.pl>
2017-10-29 23:52:33 +0100

    Zadanie 3: checkWins()

--- zad3/zad3.php
+++ zad3/zad3.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Function gets historical results of prepared txt file in the same directory.
+ * It returns and arra of strings.
+ * @return mixed
+ */
 function getResults() {
     $fileArr = explode("\n", file_get_contents('dl.txt'));
     
@@ -12,6 +17,11 @@ function getResults() {
     return $results;
 }
 
+/**
+ * Function generates 6 numbers in range from 1 to 46 with no repetition.
+ * It returns an array of strings to match the format of the 'results' array.
+ * @return mixed
+ */
 function generateCombinations() {
     $numbers = range(1,46);
     for ($i = 0; $i < 10000; $i++) {
@@ -23,15 +33,22 @@ function generateCombinations() {
     return $combinatons;
 }
 
+/**
+ * Function checks the number of occurrences of sixes from combination table in results table.
+ * It returns int and echoes the winning combinations.
+ * @param array $combinations
+ * @param array $results
+ * @return int
+ */
 function checkWins(array $combinations, array $results) {
     $combinations = array_flip($combinations);
-    $results = array_flip($results);
+    $results = array_flip($results);    //change keys with values
     
     $counter = 0;
     
     foreach ($combinations as $combination => $value) {
-        if (isset($results[$combination])) {
-            ++$counter;
+        if (isset($results[$combination])) {    //if there is a key that matches our combination
+            ++$counter;                         //increase the counter and echo the combination
             echo $counter . ' Win! Combination: '. $combination .'<br>';
         }
     }
